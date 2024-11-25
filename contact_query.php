b2b8ef66925133c5d9bf2c2e5fe307e9763ed1ea
Chaz Ortiz <chaz.ortiz@gmail.com>
2024-11-24 18:32:47 -0600

    Initial Commit

---
+++ contact_query.php
@@ -0,0 +1,19 @@
+<?php
+	include("functions.php");
+	$dblink=db_connect("contact_data");
+	$sql="SELECT * FROM `form_data` WHERE 1";
+	$results=$dblink->query($sql) or 
+		die("<h2>Something went wrong with $sql</h2>".$dblink->error);
+	while ($data=$results->fetch_array(MYSQLI_ASSOC))
+	{
+		echo '<tr>';
+		echo '<td>'.$data['first_name'].'</td>';
+		echo '<td>'.$data['last_name'].'</td>';
+		echo '<td>'.$data['email'].'</td>';
+		echo '<td>'.$data['phone'].'</td>';
+		echo '<td>'.$data['username'].'</td>';
+		echo '<td>'.$data['password'].'</td>';
+		echo '<td>'.$data['comments'].'</td>';
+		echo '</tr>';
+	}
+?> 
---
+++ home1.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+include("functions.php");
+?>
+<!doctype html>
+<html>
+	<head>
+	  <meta charset="utf-8">
+	  <title>PROJECT GROUP 10 | UPCOMING EVENTS</title>
+	  <link href="assets/css/bootstrap.css" rel="stylesheet">
+	  <link href="assets/css/justified-nav.css" rel="stylesheet">
+	  <link href="assets/css/styles.css" rel="stylesheet">
+	  <style>
+		.table-container {
+		  display: flex;
+		  justify-content: center;
+		  margin: 0 auto;
+		  padding: 20px;
+		}
+		table {
+		  width: 60%; 
+		  text-align: center;
+		  border-spacing: 0; 
+		}
+		th, td {
+		  padding: 15px;
+		  vertical-align: middle;
+		}
+	  </style>
+	</head>
+	<body>
+  <div class="container-fluid">
+    <div class="header masthead">
+      <h3 class="text-muted">Software Engineering Group Project</h3>
+      <nav>
+        <ul class="nav nav-justified">
+          <li class="nav-item active"><a class="nav-link" href="home1.php">UPCOMING EVENTS</a></li>
+		  <li class="nav-item"><a class="nav-link" href="about.html">ABOUT US</a></li>
+		  <li class="nav-item"><a class="nav-link" href="location.html">LOCATION</a></li>
+		  <li class="nav-item"><a class="nav-link" href="contact-new3.php">ADMIN LOGIN</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="content">
+      <div class="jumbotron">
+        <h3>FILLER</h3>
+        <h3>FILLER</h3>
+      </div>
+      <div class="jumbotron">
+        <h2>UPCOMING EVENTS</h2>
+      </div>
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="table-responsive table-container">
+                    <table class="table text-center">
+					  <thead>
+						<tr>
+						  <tr>
+							  <th style="text-align: center;">Performer Name</th>
+							  <th style="text-align: center;">Date</th>
+						  </tr>
+						</tr>
+					  </thead>
+           				<tbody id="form_info"> 
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div> 
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="assets/js/add-content.js"></script>
+    <script> 
+        function refresh_data() {
+            $.ajax({
+                type: 'get',
+                url: 'https://ec2-3-18-111-72.us-east-2.compute.amazonaws.com/SEproject1030/contact_query.php',
+                success: function(data) {
+                    $('#form_info').html(data);
+                }
+            });
+        }
+        setInterval(function() { refresh_data(); }, 500);
+    </script>
